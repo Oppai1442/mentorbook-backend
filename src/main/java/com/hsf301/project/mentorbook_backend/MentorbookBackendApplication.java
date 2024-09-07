3ba988cec1349871f4b2f9b4df068233191bf381
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-07 08:29:03 +0700

    Initialize backend base

---
+++ src/main/java/com/hsf301/project/mentorbook_backend/MentorbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hsf301.project.mentorbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorbookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsf301/project/mentorbook_backend/MentorbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsf301.project.mentorbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorbookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
