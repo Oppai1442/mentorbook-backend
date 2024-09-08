0141077ea70ff3e716c0f3dc5ad244a32bb9bbf5
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-09 03:20:37 +0700

    Move from com.hsf301.project.MentorBook-Backend to com.hsf301.project

--- src/main/java/com/hsf301/project/MentorbookBackendApplication.java
+++ src/main/java/com/hsf301/project/MentorbookBackendApplication.java
@@ -1,4 +1,4 @@
-package com.hsf301.project.mentorbook_backend;
+package com.hsf301.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
