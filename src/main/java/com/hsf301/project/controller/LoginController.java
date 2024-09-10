ee285778461883ccc336675f1931d7264ddc97fc
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-10 19:12:15 +0700

    Create login API

---
+++ src/main/java/com/hsf301/project/controller/LoginController.java
@@ -0,0 +1,37 @@
+package com.hsf301.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class LoginController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        //TODO: process POST request
+
+        if ("user".equals(loginRequest.getUsername()) && "password".equals(loginRequest.getPassword())) {
+            return ResponseEntity.ok("Login successful!");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class LoginRequest {
+        private String username;
+        private String password;
+    }
+
+}
