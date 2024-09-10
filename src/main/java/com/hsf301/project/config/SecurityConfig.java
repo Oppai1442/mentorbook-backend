8b2a874e62a8907954bb6b60bcf7eae035d45e55
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-10 19:07:05 +0700

    Change to permit for any request

--- src/main/java/com/hsf301/project/config/SecurityConfig.java
+++ src/main/java/com/hsf301/project/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -15,10 +16,20 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated()
-            )
-            .formLogin(withDefaults())
-            .httpBasic(withDefaults());
+                .anyRequest().permitAll() // Cho phép truy cập vào tất cả các yêu cầu
+            );
         return http.build();
     }
+
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .authorizeHttpRequests(authorize -> authorize
+    //             .requestMatchers("/", "/api/list", "/not-found-api").permitAll() // Allow access to / and /api/list
+    //             .anyRequest().authenticated() // Require authentication for all other requests
+    //         )
+    //         .formLogin(withDefaults())
+    //         .httpBasic(withDefaults()); // Custom entry point for redirection;
+    //     return http.build();
+    // }
 }
