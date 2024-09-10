f65d3048509dccf361ed4da7d1e82fbe500e8bd8
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-10 19:11:28 +0700

    Add a test API

--- src/main/java/com/hsf301/project/controller/ApiController.java
+++ src/main/java/com/hsf301/project/controller/ApiController.java
@@ -2,12 +2,15 @@ package com.hsf301.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api")
@@ -28,5 +31,10 @@ public class ApiController {
             apis.put(key.toString(), value.toString());
         });
         return apis;
+    }
+
+    @GetMapping("/send/{number}")
+    public int sendNumber(@PathVariable int number) {
+        return number + 10; // Trả về số cộng thêm 10
     }
 }
