cbb702719f379543d8f1aabc07722c63b3f637d3
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-10 19:08:58 +0700

    Add annotation @Repository

--- src/main/java/com/hsf301/project/repository/UserRepository.java
+++ src/main/java/com/hsf301/project/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.hsf301.project.repository;
 
 import com.hsf301.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
