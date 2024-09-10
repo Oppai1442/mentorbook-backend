6133f3bfeed32c17075889c80317b2f1f3cef13b
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-09-10 19:09:31 +0700

    Remove autoconfgi from spring application

--- src/main/java/com/hsf301/project/MentorbookBackendApplication.java
+++ src/main/java/com/hsf301/project/MentorbookBackendApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @Configuration
+// @EnableAutoConfiguration  // Sprint Boot Auto Configuration
+// @ComponentScan(basePackages = "com.hsf301.project")
 public class MentorbookBackendApplication {
 
 	public static void main(String[] args) {
