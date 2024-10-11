4ad8fec66944100042bb77472a68c72786d6db01
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-11 21:49:07 +0700

    Moved SecurityConfig: Relocated from .config to .config.security

--- src/main/java/com/hsf301/project/config/Security/SecurityConfig.java
+++ src/main/java/com/hsf301/project/config/Security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hsf301.project.config;
+package com.hsf301.project.config.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
