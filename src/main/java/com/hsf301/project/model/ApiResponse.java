3c8d0c5e289929ea25bb39a179ca5c93712e18ed
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-11 19:17:27 +0700

    Create New Model: ApiResponse

---
+++ src/main/java/com/hsf301/project/model/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.hsf301.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private T data;
+    private int statusCode;
+}
