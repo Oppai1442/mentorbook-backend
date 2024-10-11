09e47d1f1219643a4515674f580fa22866ecf39c
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-12 00:16:36 +0700

    Updated LoginController: Response now includes token and user data upon successful authentication

--- src/main/java/com/hsf301/project/controller/LoginController.java
+++ src/main/java/com/hsf301/project/controller/LoginController.java
@@ -5,7 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.hsf301.project.model.ApiResponse;
+import com.hsf301.project.model.CustomResponse;
+import com.hsf301.project.model.ErrorResponse;
 import com.hsf301.project.model.LoginRequest;
+import com.hsf301.project.model.TokenResponse;
 import com.hsf301.project.model.User;
 import com.hsf301.project.service.JwtService;
 import com.hsf301.project.service.UserService;
@@ -20,15 +24,17 @@ public class LoginController {
     @Autowired
     private JwtService jwtService;
 
+    @SuppressWarnings("rawtypes")
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        User user = userService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
-
+    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest loginRequest) {
+        User user = userService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+       
         if (user != null) {
             String token = jwtService.generateToken(user);
-            return ResponseEntity.ok(token);
+            return ResponseEntity.ok(new ApiResponse<>(new TokenResponse(token), new CustomResponse("user", user)));
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                                 .body(new ApiResponse<>(new ErrorResponse("Invalid credentials")));
         }
     }
 }
