a61bb8de9c4fdcfd829e002c53e6bb0d38b6d5d7
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-11 19:01:21 +0700

    Revise CSRF Controller: Add Response Handling for CSRF Status

--- src/main/java/com/hsf301/project/controller/CsrfController.java
+++ src/main/java/com/hsf301/project/controller/CsrfController.java
@@ -1,16 +1,30 @@
 package com.hsf301.project.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hsf301.project.model.ApiResponse;
+import com.hsf301.project.model.ErrorResponse;
+import com.hsf301.project.model.TokenResponse;
+
 import org.springframework.security.web.csrf.CsrfToken;
 
 @RestController
-@RequestMapping("/csrf") // Hoặc một đường dẫn khác mà bạn muốn
+@RequestMapping("/csrf")
 public class CsrfController {
 
     @GetMapping("/generate-token")
-    public CsrfToken csrf(CsrfToken token) {
-        return token; // Trả về token CSR
+    public ResponseEntity<ApiResponse<Object>> csrf(CsrfToken token) {
+        if (token != null) {
+            return ResponseEntity.ok(new ApiResponse<>(new TokenResponse(token.getToken()), HttpStatus.OK.value()));
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(new ErrorResponse("Unable to generate CSRF token"),
+                            HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        }
     }
+
 }
