2f9ce448992da12e9cedacb762b0ee8c55e21eda
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 07:33:13 +0700

    Updated ApiResponse: Changed data type of data from Object to generic T

--- src/main/java/com/hsf301/project/model/ApiResponse.java
+++ src/main/java/com/hsf301/project/model/ApiResponse.java
@@ -1,22 +1,10 @@
 package com.hsf301.project.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class ApiResponse<T> {
-    private List<Object> data;
-
-    public ApiResponse(Object... data) {
-        this.data = new ArrayList<>();
-        for (Object item : data) {
-            this.data.add(item);
-        }
-    }
+public class ApiResponse <T> {
+    private T data;
 }
