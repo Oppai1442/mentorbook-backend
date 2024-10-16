6459811bf9dc089a329b9930ffdf9225aa895c03
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 07:46:01 +0700

    Updated ApiResponse: Added @NoArgsConstructor annotation

--- src/main/java/com/hsf301/project/model/ApiResponse.java
+++ src/main/java/com/hsf301/project/model/ApiResponse.java
@@ -2,8 +2,10 @@ package com.hsf301.project.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponse <T> {
     private T data;
