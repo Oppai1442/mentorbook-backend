64f71cec389b8f14d09b9efd2442dafc2c354fd5
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 08:26:00 +0700

    Updated Utils: Added hashGenerate method

--- src/main/java/com/hsf301/project/utils/Utils.java
+++ src/main/java/com/hsf301/project/utils/Utils.java
@@ -1,5 +1,21 @@
 package com.hsf301.project.utils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Utils {
-    
+    public String hashGenerate(String input) throws NoSuchAlgorithmException{
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hashBytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
 }
