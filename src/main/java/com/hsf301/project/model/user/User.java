65f60d04ae7ca331a075f3e5c13e233fe26e1ee9
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 07:41:04 +0700

    Refactored: Moved user from .model to .model.User

---
+++ src/main/java/com/hsf301/project/model/user/User.java
@@ -0,0 +1,44 @@
+package com.hsf301.project.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "Users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String fullName;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(length = 15)
+    private String phone;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @Column(length = 255)
+    private String avatar = "avatar.png";
+}
