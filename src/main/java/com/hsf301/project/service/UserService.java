0c9dbe55c7c9d76a2ad6646aa43964e8c8348939
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 07:43:07 +0700

    Updated UserService: Added method to handle requests from API

--- src/main/java/com/hsf301/project/service/UserService.java
+++ src/main/java/com/hsf301/project/service/UserService.java
@@ -1,9 +1,15 @@
 package com.hsf301.project.service;
 
-import com.hsf301.project.model.User;
+import com.hsf301.project.exception.EmailConflictException;
+import com.hsf301.project.model.SignupRequest;
+import com.hsf301.project.model.user.User;
 import com.hsf301.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
 
 @Service
 public class UserService {
@@ -16,6 +22,31 @@ public class UserService {
         if (user != null && user.getPassword().equals(password)) {
             return user;
         }
+        return null;
+    }
+
+    private User getAccount(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity register(SignupRequest signupRequest) {
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            throw new EmailConflictException(null);
+            // return ResponseEntity.status(HttpStatus.CONFLICT).body("Email is already in use");
+        }
+
+        User user = new User();
+        user.setUsername("null");
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(signupRequest.getPassword());
+        user.setFullName(signupRequest.getFullName());
+        user.setPhone(signupRequest.getPhone());
+        user.setRole("user");
+        user.setCreatedDate(LocalDateTime.now());
+
+        userRepository.save(user);
+
         return null;
     }
 }
