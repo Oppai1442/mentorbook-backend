f5e83134310467fa30ba08b6386d7979ffbb324e
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 07:44:04 +0700

    Created UserResponse: Added class to contain user data for client return, excluding sensitive information

---
+++ src/main/java/com/hsf301/project/model/user/UserResponse.java
@@ -0,0 +1,24 @@
+package com.hsf301.project.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Integer id;
+    private String username;
+    private String fullName;
+    private String email;
+    private String role;
+
+    public UserResponse(User user) {
+        this.id = user.getUserId();
+        this.username = user.getUsername();
+        this.fullName = user.getFullName();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+    }
+}
