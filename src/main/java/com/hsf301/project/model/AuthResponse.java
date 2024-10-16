ccda66ac04a1e984d1b914140c9c3b61da787be2
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-16 07:46:48 +0700

    Created AuthResponse: Added class to contain user data and account token

---
+++ src/main/java/com/hsf301/project/model/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.hsf301.project.model;
+
+import com.hsf301.project.model.user.UserResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private UserResponse user;
+}
