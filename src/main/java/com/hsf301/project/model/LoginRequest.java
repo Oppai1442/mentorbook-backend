5102996b391a2dfb5e3fee0bdf6a60d64f77f3d2
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-18 20:38:07 +0700

    Updated LoginRequest: Added @Valid and @NotNull annotations (with messages)

--- src/main/java/com/hsf301/project/model/LoginRequest.java
+++ src/main/java/com/hsf301/project/model/LoginRequest.java
@@ -1,9 +1,17 @@
 package com.hsf301.project.model;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+    @NotNull(message = "email is required")
     private String email;
+    
+    @NotNull(message = "password is required")
     private String password;
 }
