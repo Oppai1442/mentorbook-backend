5d4bb27dc6f4ff2ff424abf6aa24eba6587e982d
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-19 07:57:43 +0700

    Updated UserResponse: Removed username and added avatar field

--- src/main/java/com/hsf301/project/model/user/UserResponse.java
+++ src/main/java/com/hsf301/project/model/user/UserResponse.java
@@ -4,20 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import com.hsf301.project.service.ImageService;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponse {
     private Integer id;
-    private String username;
     private String fullName;
     private String email;
     private String role;
+    private byte[] avatar;
 
-    public UserResponse(User user) {
+    public UserResponse(User user, ImageService imageService) {
         this.id = user.getUserId();
         this.fullName = user.getFullName();
         this.email = user.getEmail();
         this.role = user.getRole();
+        this.avatar = imageService.getImage(user.getAvatar());
     }
 }
