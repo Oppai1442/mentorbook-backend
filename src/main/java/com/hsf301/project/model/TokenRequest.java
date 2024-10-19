358ba1ee77310dc1bbf202e0a3fb6a3f4cece562
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-20 01:02:37 +0700

    Created TokenRequest: Added class for handling token requests

---
+++ src/main/java/com/hsf301/project/model/TokenRequest.java
@@ -0,0 +1,14 @@
+package com.hsf301.project.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequest {
+    @NotNull(message = "Token is required")
+    String token;
+}
