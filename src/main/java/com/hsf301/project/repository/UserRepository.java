d7194259ea43fd708c916c221998c2b9e480e512
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-26 12:06:39 +0700

    Updated UserRepository: Added findByRole method

--- src/main/java/com/hsf301/project/repository/UserRepository.java
+++ src/main/java/com/hsf301/project/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.hsf301.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List; // Add this import
 
 import com.hsf301.project.model.user.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String username);
     boolean existsByEmail(String email);
+    List<User> findByRole(String role);
 }
