d0fcf636d48fe9e266063ae508b2021bbfb6176f
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-28 01:55:33 +0700

    Updated UserRepository: Added findByRole and findFullNameContainingIgnoreCase methods

--- src/main/java/com/hsf301/project/repository/UserRepository.java
+++ src/main/java/com/hsf301/project/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.hsf301.project.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List; // Add this import
@@ -11,4 +13,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String username);
     boolean existsByEmail(String email);
     List<User> findByRole(String role);
+    
+    @Override
+    Page<User> findAll(Pageable pageable);
+    Page<User> findByRole(String role, Pageable pageable);
+
+    // Phương thức tìm mentor theo tiêu chí lọc (ví dụ: theo tên)
+    Page<User> findByFullNameContainingIgnoreCase(String filter, Pageable pageable);
 }
