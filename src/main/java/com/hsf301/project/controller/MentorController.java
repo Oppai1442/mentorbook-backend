fc87374a47e529dde2655566db93e6c3ec49df1f
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 05:01:29 +0700

    Updated MentorController: Changed API endpoint /get-all-mentors to use POST method

--- src/main/java/com/hsf301/project/controller/MentorController.java
+++ src/main/java/com/hsf301/project/controller/MentorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.hsf301.project.model.request.MentorRequest;
 import com.hsf301.project.model.response.ApiResponse;
 import com.hsf301.project.model.response.MentorResponse;
 import com.hsf301.project.service.MentorService;
@@ -17,10 +18,10 @@ public class MentorController {
     @Autowired
     private MentorService mentorService;
 
-    @GetMapping("/get-all-mentors")
-    public ResponseEntity getMethodName() {
-        List<MentorResponse> mentors = mentorService.getAllMentors();
-        return ResponseEntity.ok(new ApiResponse(mentors));
+    @PostMapping("/get-all-mentors")
+    public ResponseEntity<ApiResponse<List<MentorResponse>>> getMentors(@RequestBody MentorRequest data) {
+        List<MentorResponse> mentors = mentorService.getMentors(data);
+        System.out.println(data);
+        return ResponseEntity.ok(new ApiResponse<List<MentorResponse>>(mentors));
     }
-
 }
