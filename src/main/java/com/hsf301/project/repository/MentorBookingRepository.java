edb4e341cfe5f2f275b03e02cdfb88fe347f114c
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:59:29 +0700

    Created MentorBookingRepository: Added repository for managing mentor booking data

---
+++ src/main/java/com/hsf301/project/repository/MentorBookingRepository.java
@@ -0,0 +1,16 @@
+package com.hsf301.project.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hsf301.project.model.mentorBooking.MentorBooking;
+import com.hsf301.project.model.user.User;
+
+
+@Repository
+public interface MentorBookingRepository extends JpaRepository<MentorBooking, Integer> {
+    List<MentorBooking> findByMentor_UserIdAndStatus(Integer mentorId, String status);
+    List<MentorBooking> findByUserAndStatus(User user, String status);
+}
