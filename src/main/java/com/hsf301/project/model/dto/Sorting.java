780f22c9a6972539a5f3d3cee1ec103abebddd5e
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:55:55 +0700

    Created Sorting class in DTO: Added class for handling sorting functionality in data transfer objects

---
+++ src/main/java/com/hsf301/project/model/dto/Sorting.java
@@ -0,0 +1,15 @@
+package com.hsf301.project.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sorting {
+    private String name;
+    private String experience;
+    private String rating;
+    private String price;
+}
