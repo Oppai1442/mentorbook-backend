b7d6945ad18926281f937846f86dee11a65f7154
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-10-29 04:56:10 +0700

    Created Price class in DTO: Added class for managing price-related data in data transfer objects

---
+++ src/main/java/com/hsf301/project/model/dto/Price.java
@@ -0,0 +1,14 @@
+package com.hsf301.project.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Price {
+    private Integer min;
+    private Integer max;
+}
+
