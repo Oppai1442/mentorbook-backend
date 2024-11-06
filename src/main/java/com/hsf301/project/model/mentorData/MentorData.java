9508268aa5d6402e8bb0c8c4bea94acd66643ef3
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 05:01:36 +0700

    Updated MentorData model: Reconfigured @Column annotations for database compatibility

--- src/main/java/com/hsf301/project/model/mentorData/MentorData.java
+++ src/main/java/com/hsf301/project/model/mentorData/MentorData.java
@@ -23,18 +23,24 @@ public class MentorData {
     @JoinColumn(name = "mentor_id", referencedColumnName = "user_id")
     private User mentor;
 
-    @Column(name = "mentor_skills", nullable = false, length = 100)
+    @Column(name = "mentor_skills", nullable = false)
     private String mentorSkills = "[]";
 
-    @Column(name = "mentor_experience", nullable = false, length = 100)
+    @Column(name = "mentor_experience", nullable = false)
     private Integer mentorExperience = 0;
 
-    @Column(name = "mentor_role", nullable = false, length = 100)
+    @Column(name = "mentor_role", nullable = false, columnDefinition = "nvarchar(max)")
     private String mentorRole = "";
     
-    @Column(name = "mentor_rating", nullable = false, length = 1)
+    @Column(name = "mentor_rating", nullable = false, columnDefinition = "decimal(3,2)")
     private Double mentorRating = 0.0;
     
-    @Column(name = "mentor_price_per_hour", nullable = false)
+    @Column(name = "mentor_price_per_hour", nullable = false, columnDefinition = "decimal(10,2)")
     private Double mentorPrice = 0.0;
+    
+    @Column(name = "mentor_description", length = 255)
+    private String mentorDescription;
+
+    @Column(name = "mentor_available_time", nullable = false, columnDefinition = "nvarchar(max)")
+    private String mentorAvailableTime = "[]";
 }
