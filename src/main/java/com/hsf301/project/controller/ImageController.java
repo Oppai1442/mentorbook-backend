21ea2a8d34efe11c960ec72eea6936ee2f0b8c5f
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 04:42:40 +0700

    Updated Image: Created API and function to handle requests for uploading a single image

--- src/main/java/com/hsf301/project/controller/ImageController.java
+++ src/main/java/com/hsf301/project/controller/ImageController.java
@@ -1,66 +1,62 @@
 package com.hsf301.project.controller;
 
+import com.hsf301.project.exception.FileUploadException;
+import com.hsf301.project.model.response.ApiResponse;
+import com.hsf301.project.model.response.ImageUrl;
+import com.hsf301.project.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-
 import java.security.NoSuchAlgorithmException;
-import java.util.Map;
-
-import com.hsf301.project.exception.FileUploadException;
-import com.hsf301.project.service.ImageService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/images")
 public class ImageController {
 
-    @Value("${ImageController.saveLocation}")
-    private String UPLOAD_DIR;
-
     @Autowired
     private ImageService imageService;
 
+    // Xử lý upload một ảnh duy nhất
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile[] files) {
+    public ResponseEntity<ApiResponse<ImageUrl>> uploadSingleImage(@RequestParam("image") MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new FileUploadException("No file uploaded.");
+        }
+    
+        try {
+            String imageUrl = imageService.saveSingleImageAndGetUrl(file);
+            ImageUrl imageUrlResponse = new ImageUrl(imageUrl);
+            return ResponseEntity.ok(new ApiResponse<ImageUrl>(imageUrlResponse));
+        } catch (IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(new ApiResponse<>(null));
+        }
+    }
+    
+
+    // Xử lý upload nhiều ảnh
+    @PostMapping("/uploads")
+    public ResponseEntity<ApiResponse<List<String>>> uploadMultipleImages(@RequestParam("image") MultipartFile[] files) {
         if (files.length == 0) {
             throw new FileUploadException("No files uploaded.");
         }
 
-        StringBuilder responseMessage = new StringBuilder();
-
         try {
-            String[] filePaths = imageService.saveImages(files);
-            for (String filePath : filePaths) {
-                responseMessage.append("File uploaded successfully: ").append(filePath).append("\n");
-            }
+            List<String> imageUrls = imageService.saveMultipleImagesAndGetUrls(files);
+            return ResponseEntity.ok(new ApiResponse<>(imageUrls));
         } catch (IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
-            responseMessage.append("Failed to upload files.\n");
+            return ResponseEntity.status(500).body(new ApiResponse<>(null));
         }
-
-        return ResponseEntity.ok(responseMessage.toString());
     }
 
+    // Lấy ảnh theo tên tệp
     @GetMapping("/uploads/{filename}")
     public ResponseEntity<Object> getImage(@PathVariable String filename) {
-        Map<String, Object> data = imageService.getImage(filename, false);
-
-        return ResponseEntity.ok()
-                                .contentType(MediaType.parseMediaType((String) data.get("MediaType")))
-                                .body(data.get("image"));
-    }
-
-    @GetMapping("/id/{fileName:.+}")
-    public ResponseEntity<Object> getImageUrl(@PathVariable String fileName) {
-        Map<String, Object> data = imageService.getImage(fileName, false);
-
-        return ResponseEntity.ok()
-                                .contentType(MediaType.parseMediaType((String) data.get("MediaType")))
-                                .body(data.get("image"));
+        return imageService.getImageAsResponseEntity(filename);
     }
 }
--- src/main/java/com/hsf301/project/service/ImageService.java
+++ src/main/java/com/hsf301/project/service/ImageService.java
@@ -1,87 +1,93 @@
 package com.hsf301.project.service;
 
+import com.hsf301.project.exception.FileUploadException;
+import com.hsf301.project.exception.ImageLoadException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.hsf301.project.exception.FileUploadException;
-import com.hsf301.project.exception.ImageLoadException;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
-import java.util.Map;
-import java.util.HashMap;
-import java.util.UUID;
-
-
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ImageService {
+
     @Value("${ImageController.saveLocation}")
     private String UPLOAD_DIR;
 
-    public String[] saveImages(MultipartFile[] files) throws IOException, NoSuchAlgorithmException {
-        String[] filePaths = new String[files.length];
-
-        for (int i = 0; i < files.length; i++) {
-            filePaths[i] = saveImage(files[i]);
-        }
-
-        return filePaths;
+    // Lưu một ảnh duy nhất và trả về URL
+    public String saveSingleImageAndGetUrl(MultipartFile file) throws IOException, NoSuchAlgorithmException {
+        String fileName = saveImage(file);
+        return getImageUrl(fileName);
     }
 
+    // Lưu nhiều ảnh và trả về danh sách URL
+    public List<String> saveMultipleImagesAndGetUrls(MultipartFile[] files) throws IOException, NoSuchAlgorithmException {
+        List<String> imageUrls = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String fileName = saveImage(file);
+            imageUrls.add(getImageUrl(fileName));
+        }
+        return imageUrls;
+    }
+
+    // Lưu ảnh và trả về tên file
     private String saveImage(MultipartFile file) throws IOException, NoSuchAlgorithmException {
         String originalFilename = file.getOriginalFilename();
-
+    
         if (originalFilename == null || originalFilename.isEmpty()) {
             throw new FileUploadException("File name is missing.");
         }
-
+    
         String extension = "";
         int lastDotIndex = originalFilename.lastIndexOf('.');
         if (lastDotIndex >= 0) {
             extension = originalFilename.substring(lastDotIndex);
         }
-
+    
         String baseName = originalFilename.substring(0, lastDotIndex >= 0 ? lastDotIndex : originalFilename.length());
-
         String newFileName = UUID.randomUUID().toString() + "@" + baseName + extension;
-
+    
         File uploadDir = new File(UPLOAD_DIR);
         if (!uploadDir.exists()) {
             uploadDir.mkdirs();
         }
+    
         File destinationFile = new File(uploadDir, newFileName);
         file.transferTo(destinationFile);
-
-        return destinationFile.getAbsolutePath();
-    }
-
-    public byte[] getImage(String filename) {
-        File file = new File(UPLOAD_DIR + filename);
-        if (!file.exists()) {
-            throw new ImageLoadException(filename + " does not exist");
-        }
     
-        try {
-            return Files.readAllBytes(file.toPath());
-        } catch (IOException e) {
-            throw new ImageLoadException("Failed to load image data for " + filename, e);
-        }
+        return newFileName;
     }
 
-    public String getImageUrl(String image) {
+    // Tạo URL truy cập ảnh
+    public String getImageUrl(String fileName) {
         return ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/api/images/id/")
-                .path(image)
+                .path("/api/images/uploads/")
+                .path(fileName)
                 .toUriString();
     }
 
-    public Map<String, Object> getImage(String filename, Boolean fileExtension) {
+    // Lấy ảnh dưới dạng `ResponseEntity` cho HTTP response
+    public ResponseEntity<Object> getImageAsResponseEntity(String filename) {
+        Map<String, Object> data = getImageData(filename, false);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType((String) data.get("MediaType")))
+                .body(data.get("image"));
+    }
+
+    // Lấy dữ liệu ảnh từ tên tệp
+    private Map<String, Object> getImageData(String filename, Boolean includeExtension) {
         File file = new File(UPLOAD_DIR + filename);
         Map<String, Object> data = new HashMap<>();
 
@@ -95,7 +101,7 @@ public class ImageService {
             byte[] imageData = Files.readAllBytes(file.toPath());
             data.put("image", imageData);
 
-            if (fileExtension) {
+            if (includeExtension) {
                 String extension = getFileExtension(filename);
                 data.put("extension", extension);
             }
