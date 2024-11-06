d0a2ebd4602fb2c34ce31ee6d82cce009b8af834
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 04:46:04 +0700

    Updated UserRepository: Added findByUserId method

--- src/main/java/com/hsf301/project/repository/UserRepository.java
+++ src/main/java/com/hsf301/project/repository/UserRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List; // Add this import
+import java.util.List;
 
 import com.hsf301.project.model.user.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserId(Long userId);
     User findByEmail(String username);
     boolean existsByEmail(String email);
     List<User> findByRole(String role);
@@ -18,6 +19,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Page<User> findAll(Pageable pageable);
     Page<User> findByRole(String role, Pageable pageable);
 
-    // Phương thức tìm mentor theo tiêu chí lọc (ví dụ: theo tên)
     Page<User> findByFullNameContainingIgnoreCase(String filter, Pageable pageable);
 }
