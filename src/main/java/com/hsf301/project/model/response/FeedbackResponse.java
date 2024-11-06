a195675637bd730ab3f23f2d725e8b196d026293
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 04:51:26 +0700

    Created FeedbackResponse: Added model for handling feedback response data

---
+++ src/main/java/com/hsf301/project/model/response/FeedbackResponse.java
@@ -0,0 +1,32 @@
+package com.hsf301.project.model.response;
+
+import com.hsf301.project.model.feedback.Feedback;
+import com.hsf301.project.service.ImageService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedbackResponse {
+    private Integer feedbackId;
+    private String comment;
+    private Integer rating;
+    private LocalDateTime feedbackTime;
+    private Integer userId;
+    private String fullName;
+    private String userAvatarUrl;
+
+    public FeedbackResponse(Feedback feedback, ImageService imageService) {
+        this.feedbackId = feedback.getFeedbackId();
+        this.comment = feedback.getComment();
+        this.rating = feedback.getRating();
+        this.feedbackTime = feedback.getFeedbackTime();
+        this.userId = feedback.getBooking().getUser().getUserId();
+        this.fullName = feedback.getBooking().getUser().getFullName();
+        this.userAvatarUrl = imageService.getImageUrl(feedback.getBooking().getUser().getAvatar());
+    }
+}
