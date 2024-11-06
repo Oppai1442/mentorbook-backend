b1bc5f412e63d1a96389c3c1c0dcd8409673abde
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 05:02:39 +0700

    Updated MentorResponseList: Removed getter/setter methods and replaced them with Lombok annotations

--- src/main/java/com/hsf301/project/model/response/MentorResponseList.java
+++ src/main/java/com/hsf301/project/model/response/MentorResponseList.java
@@ -2,20 +2,14 @@ package com.hsf301.project.model.response;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MentorResponseList {
     private List<MentorResponse> mentors;
     private int totalFound;
-
-    public MentorResponseList(List<MentorResponse> mentors, int totalFound) {
-        this.mentors = mentors;
-        this.totalFound = totalFound;
-    }
-
-    public List<MentorResponse> getMentors() {
-        return mentors;
-    }
-
-    public int getTotalFound() {
-        return totalFound;
-    }
 }
