f2ed242f061f4eb9866112f2515e5b4d22234f21
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 05:04:25 +0700

    Updated UserResponse: Added fields for backgroundUrl, phone, birthdate, and gender

--- src/main/java/com/hsf301/project/model/response/UserResponse.java
+++ src/main/java/com/hsf301/project/model/response/UserResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 import com.hsf301.project.model.user.User;
 import com.hsf301.project.service.ImageService;
 
@@ -16,6 +18,10 @@ public class UserResponse {
     private String email;
     private String role;
     private String avatarUrl;
+    private String backgroundUrl;
+    private String phone;
+    private LocalDate birthDate;
+    private String gender;
 
     public UserResponse(User user, ImageService imageService) {
         this.id = user.getUserId();
@@ -23,5 +29,9 @@ public class UserResponse {
         this.email = user.getEmail();
         this.role = user.getRole();
         this.avatarUrl = imageService.getImageUrl(user.getAvatar());
+        this.backgroundUrl = imageService.getImageUrl(user.getBackground());
+        this.phone = user.getPhoneNumber();
+        this.birthDate = user.getBirthDate();
+        this.gender = user.getGender();
     }
 }
