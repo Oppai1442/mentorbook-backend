d7ca7ef017ecd2f9a35d5e0963edd6798500ce65
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-11-07 05:00:00 +0700

    Updated MentorController: Temporarily disabled API endpoint /get-mentors-data/{id}

--- src/main/java/com/hsf301/project/controller/MentorController.java
+++ src/main/java/com/hsf301/project/controller/MentorController.java
@@ -1,14 +1,11 @@
 package com.hsf301.project.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.hsf301.project.model.request.MentorRequest;
 import com.hsf301.project.model.response.ApiResponse;
-import com.hsf301.project.model.response.MentorResponse;
 import com.hsf301.project.model.response.MentorResponseList;
 import com.hsf301.project.service.MentorService;
 
@@ -25,4 +22,14 @@ public class MentorController {
         
         return ResponseEntity.ok(new ApiResponse<MentorResponseList>(mentors));
     }
+
+    // @GetMapping("/get-mentor-data/{mentorId}")
+    // public ResponseEntity<MentorDetails> getMentorData(@PathVariable String mentorId) {
+    //     try {
+    //         MentorDetails mentorDetails = mentorService.getMentorData(mentorId);
+    //         return ResponseEntity.ok(mentorDetails);
+    //     } catch (Exception e) {
+    //         return ResponseEntity.status(404).body(null); // or handle error response as needed
+    //     }
+    // }
 }
